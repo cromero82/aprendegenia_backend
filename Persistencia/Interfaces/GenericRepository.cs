89c3055c34ffa21081d2e70913fda65e5722795a
Carlos Romero <soportecarlosromero@gmail.com>
2020-03-15 09:58:58 -0500

    completan las interfaces y clase generica

--- Persistencia/Interfaces/GenericRepository.cs
+++ Persistencia/Interfaces/GenericRepository.cs
@@ -1,48 +1,52 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Persistencia.Entities;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace Persistencia.Interfaces
 {
-    class GenericRepository/*<T> : IGenericRepository<T> where T : class*/
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
     {
-        //internal DbContext _context;
+        internal ApplicationDbContext _context;
 
-        //public GenericRepository(DbContext context)
-        //{
-        //    _context = context;
-        //}
+        public GenericRepository(ApplicationDbContext  context)
+        {
+            _context = context;
+        }
 
-        //public void Add(T entity)
-        //{
-        //    _context.Set<T>().Add(entity);
-        //}
+        public void Add(T entity)
+        {
+            _context.Set<T>().Add(entity);
+        }
 
-        //public void Delete(T entity)
-        //{
-        //    _context.Set<T>().Remove(entity);
-        //}
+        public void Delete(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+        }
 
-        //public void Edit(T entity)
-        //{
-        //    _context.Entry(entity).State = EntityState.Modified;
-        //}
+        public void Edit(T entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+        }
 
-        //public IQueryable<T> Find(Expression<Func<T, bool>> predicate)
-        //{
-        //    IQueryable<T> query = _context.Set<T>().Where(predicate);
-        //    return query;
-        //}
+        public IQueryable<T> Find(Expression<Func<T, bool>> predicate)
+        {
+            IQueryable<T> query = _context.Set<T>().Where(predicate);
+            return query;
+        }
 
-        //public IQueryable<T> GetAll()
-        //{
-        //    IQueryable<T> query = _context.Set<T>();
-        //    return query;
-        //}
+        public IQueryable<T> GetAll()
+        {
+            IQueryable<T> query = _context.Set<T>();
+            return query;
+        }
 
-        //public void Save()
-        //{
-        //    _context.SaveChanges();
-        //}
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
     }
 }
--- Persistencia/Interfaces/IGenericRepository.cs
+++ Persistencia/Interfaces/IGenericRepository.cs
@@ -1,10 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Linq;
+using System.Linq.Expressions;
 
 namespace Persistencia.Interfaces
 {
-    public interface IGenericRepository
+    public interface IGenericRepository<T> where T : class
     {
+
+        IQueryable<T> GetAll();
+        IQueryable<T> Find(Expression<Func<T, bool>> predicate);
+        void Add(T entity);
+        void Delete(T entity);
+        void Edit(T entity);
+        void Save();
+
     }
 }
